4b0bb28f03d028d3c4833f11a57931b3cbab44f0
srujan-garnayak <srujangarnayak@gmail.com>
2025-06-28 00:54:32 +0530

    added text

--- src/components/Checking.jsx
+++ src/components/Checking.jsx
@@ -4,6 +4,7 @@ function Checking() {
   return (
     <div>
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, saepe autem recusandae facilis ullam eius dolorum sit nesciunt veniam optio voluptatibus assumenda iste aliquid dolores error consequatur fugit debitis ducimus?
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur minus fugit molestias dicta quos quasi doloribus, exercitationem aspernatur nihil adipisci animi similique aliquid ullam. Nobis porro eaque similique non repellat.
     </div>
   )
 }
