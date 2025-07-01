c43dd68e17ec4240dd8fa021757e4a687bc365c9
srujan-garnayak <srujangarnayak@gmail.com>
2025-07-01 23:50:15 +0530

    testing

--- src/components/Checking.jsx
+++ src/components/Checking.jsx
@@ -5,7 +5,6 @@ function Checking() {
     <div>
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, saepe autem recusandae facilis ullam eius dolorum sit nesciunt veniam optio voluptatibus assumenda iste aliquid dolores error consequatur fugit debitis ducimus?
       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur minus fugit molestias dicta quos quasi doloribus, exercitationem aspernatur nihil adipisci animi similique aliquid ullam. Nobis porro eaque similique non repellat.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, officiis tenetur! Obcaecati quod eaque, esse explicabo dolor placeat incidunt aspernatur dolore voluptatem qui ex! Facere fuga dolorum corrupti temporibus neque.
     </div>
   )
 }
